7d283631399f54e92ee4cbff132ac89bab090a1d
Shubham Agrawal <102228802+shubham10032@users.noreply.github.com>
2023-10-26 05:43:37 +0530

    Update db.js
    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
  
 export const Connection = async() => {
-    const URL = 'mongodb+srv://shubhamhydear:DuA1maPWba1C7j2c@cluster0.kizrale.mongodb.net/';
+    // const URL = 'mongodb+srv://shubhamhydear:DuA1maPWba1C7j2c@cluster0.kizrale.mongodb.net/';
     try {
        await mongoose.connect(URL, {useUnifiedTopology: true , useNewUrlParser: true});
        console.log("Database Connected successfully")
